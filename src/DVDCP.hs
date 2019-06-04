68aaa0f5480365d63756e1793940a0167b09f617
Shea Newton <shnewto@gmail.com>
2019-06-03 22:52:07 -0700

    Some line by line example code
    This commit is just getting some code to compile. It's almost line for line,
    pulled from the test here https://github.com/vincenthz/hs-git/blob/master/tests/Repo.hs
    
    Thought it'd work out to be a helpful example, we'll see if that's the
    case. I keep thinking I know what I'm looking at but then, I don't!
    Here's an embarassing thing, I expected that this program was going to
    print to stdout. Then it didn't. And I don't see clearly how to get it
    to :upside_down_face:

--- src/DVDCP.hs
+++ src/DVDCP.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module DVDCP
     ( someFunc
     )
@@ -22,6 +24,65 @@ import           Data.Maybe
 import           Text.Bytedump
 import           System.Exit
 
+-- During experiments, hacking on the code below, pulled from 
+-- https://github.com/vincenthz/hs-git/blob/master/tests/Repo.hs
+
+onLocalRepo f = do
+    fpath <- findRepoMaybe
+    case fpath of
+        Nothing ->
+            putStrLn
+                "cannot run this test without repository. clone the original repository for test"
+        Just _ -> Data.Git.Storage.withCurrentRepo f
+
+
+doLocalMarshallEq
+    :: Git SHA1
+    -> IO
+           [ [ Maybe
+                   ( Ref SHA1
+                   , Ref SHA1
+                   , (ObjectHeader SHA1, L.ByteString)
+                   , (ObjectHeader SHA1, L.ByteString)
+                   )
+             ]
+           ]
+
+doLocalMarshallEq git = do
+    prefixes <- looseEnumeratePrefixes (gitRepoPath git)
+    forM prefixes $ \prefix -> do
+        refs <- looseEnumerateWithPrefix (gitRepoPath git) prefix
+        forM refs $ \ref -> do
+            raw <- looseReadRaw (gitRepoPath git) ref
+            obj <- looseRead (gitRepoPath git) ref
+            let content = looseMarshall obj
+            let raw2    = looseUnmarshallRaw content
+            let hashed  = hashLBS content
+            if ref /= hashed
+                then return $ Just (ref, hashed, raw, raw2)
+                else return Nothing
+
+printDiff (actualRef, gotRef, (actualHeader, actualRaw), (gotHeader, gotRaw)) =
+    do
+        putStrLn "=========== difference found"
+        putStrLn ("ref expected: " ++ show actualRef)
+        putStrLn ("ref got     : " ++ show gotRef)
+        putStrLn ("header expected: " ++ show actualHeader)
+        putStrLn ("header got     : " ++ show gotHeader)
+        putStrLn "raw diff:"
+        putStrLn $ dumpDiffLBS actualRaw gotRaw
+
+printLocalMarshallError l
+    | null l
+    = putStrLn "local marshall:   [OK]"
+    | otherwise
+    = putStrLn ("local marshall: [" ++ show (length l) ++ " errors]")
+        >> mapM_ printDiff l
+        >> exitFailure
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- someFunc = putStrLn "someFunc"
+someFunc = do
+    onLocalRepo $ \(git :: Git SHA1) -> do
+        doLocalMarshallEq git >>= printLocalMarshallError . catMaybes . concat
+        return ()
